31180585b33ac24e24fc3a384486cea9444e3b7e
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2023-12-24 23:10:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,11 +17,12 @@ SB_key = config.get("credentials", "pdisk_key")
 # Select a server
 def select_server():
     server_url = "https://streambits.net/api/upload/server"
-    params = {"key": SB_key}
+    params = {"key": "749yn5bi434hvl9r22m"}
     response = requests.get(server_url, params=params)
     try:
         server_data = response.json()
         upload_url = server_data["result"]
+        print(upload_url)
         return upload_url
     except ValueError:
         print("Error parsing server response JSON:", response.text)
@@ -41,20 +42,22 @@ async def handle_video(client: Client, message: Message):
     if message.video or message.document:
         file_info = message.video or message.document
         file_path = await client.download_media(file_info)
-
+        print(file_info)
+        print(file_path)
         if not is_supported_file(file_path):
             return "Sorry, only video files (MP4, MOV, AVI, MKV) are supported. Please upload a video file."
 
-        file_obj = open(file_path, 'rb')
+        file_obj = open(file_path, 'r+b')
         upload_data = {
-            'key': SB_key,
-            'file': (os.path.basename(file_path), file_obj),
-            #'snapshot': formate should be file
+            "key": "749yn5bi434hvl9r22m",
+            "file": (os.path.basename(file_path), file_obj),
+            "fld_id": 0
         }
-
-        response = requests.post(upload_url, files=upload_data)
+        response = requests.get(upload_url, params=upload_data)
+        print(response)
         try:
             upload_result = response.json()[0]
+            print(upload_result)
             file_code = upload_result["filecode"]
             file_url = f"https://streambits.net/{file_code}"
             return file_url
@@ -65,15 +68,18 @@ async def handle_video(client: Client, message: Message):
             if 'file_obj' in locals():
                 # Close and delete the file
                 file_obj.close()
-                os.remove(file_path)
+                #os.remove(file_path)
 
     elif message.text:
         url = message.text.strip()
         if not url.startswith("http"):
             return "Invalid URL format. Please provide a valid URL."
-
         upload_url = "https://streambits.net/api/upload/url"
-        params = {"key": SB_key, "url": url, "fld_id": "0"}
+        params = {
+            "key": "749yn5bi434hvl9r22m", 
+            "url": url, 
+            "fld_id": 0
+            }
 
         response = requests.get(upload_url, params=params)
         try:
@@ -88,10 +94,7 @@ async def handle_video(client: Client, message: Message):
         return "Invalid file format or URL. Please send a video file or a valid URL."
 
 
-
-
-app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
-
+app = Client("my_bot", api_id=3393749, api_hash="a15a5954a1db54952eebd08ea6c68b71", bot_token="5872747581:AAH7_XPCOCEVfbgUhepjJWlcOmj8wjDTjBk")
 
 @app.on_message(filters.command("start"))
 async def handle_start_command(client: Client, message: Message):
